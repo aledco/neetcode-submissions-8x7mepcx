6137b0606e5c3b3a5985590e32c7d08ba2c34a2c
Alexander Cooper <103967988+aledco@users.noreply.github.com>
2026-05-22 11:28:12 -0700

    Add: climbing-stairs - submission-4

---
+++ Data Structures & Algorithms/climbing-stairs/submission-4.py
@@ -0,0 +1,26 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        # if n == 1:
+        #     return 1
+        # elif n == 2:
+        #     return 2
+        # else:
+            # one, two = 1, 1
+            # # M = [1, 2]
+            # for _ in range(n-1):
+            # # for i in range(2, n):
+            #     one, two = one + two, one
+            #     # M.append(
+            #     #     M[i-1] + M[i-2]
+            #     # )
+            # return one
+            # # return M[-1]
+
+        import math
+
+        b = math.sqrt(5)
+        phi = (1 + b) / 2
+        psi = (1 - b) / 2
+        return round(
+            (phi ** (n+1) - psi ** (n-1)) / b
+        )
