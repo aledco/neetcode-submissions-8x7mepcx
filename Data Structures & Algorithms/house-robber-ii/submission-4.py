00b79cad170ac4bbeef0b9879918211fa2a446a5
Alexander Cooper <103967988+aledco@users.noreply.github.com>
2026-05-23 13:02:41 -0700

    Add: house-robber-ii - submission-4

---
+++ Data Structures & Algorithms/house-robber-ii/submission-4.py
@@ -0,0 +1,37 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        
+        if len(nums) == 0:
+            return 0
+        elif len(nums) <= 2:
+            return max(nums)
+
+        def dfs(N, i=0):
+            if i >= len(N):
+                return 0
+            
+            return max(
+                N[i] + dfs(N, i+2),
+                dfs(N, i+1)
+            )
+        
+        # return  max(
+        #     dfs(nums[:-1]),
+        #     dfs(nums[1:])
+        # )
+
+        def dynamic_programming(N):
+            dp = [0] * len(N)
+            dp[0] = N[0]
+            dp[1] = max(N[0], N[1])
+            for i in range(2, len(N)):
+                dp[i] = max(
+                    dp[i-2] + N[i],
+                    dp[i-1]
+                )
+            return dp[-1]
+        
+        return max(
+            dynamic_programming(nums[:-1]),
+            dynamic_programming(nums[1:])
+        )
