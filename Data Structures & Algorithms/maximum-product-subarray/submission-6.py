08d5343e665656b9a2e2795d9c70e2c6750409ef
Alexander Cooper <103967988+aledco@users.noreply.github.com>
2026-05-24 11:31:25 -0700

    Add: maximum-product-subarray - submission-6

---
+++ Data Structures & Algorithms/maximum-product-subarray/submission-6.py
@@ -0,0 +1,55 @@
+import math
+
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        
+        def bruteForce(N):
+            m = min(N)
+            for i in range(len(N)):
+                p = 1
+                for j in range(i, len(N)):
+                    p *= N[j]
+                    m = max(m, p)
+            return m
+
+        # return bruteForce(nums)
+
+        def dynamicProgramming(N):
+            dp = [[0, 0] for _ in N]
+            dp[0] = [N[0], N[0]]
+            m = min(N)
+            for i in range(1, len(N)):
+                dp[i][0] = max(
+                    dp[i-1][0] * N[i],
+                    dp[i-1][1] * N[i],
+                    N[i]
+                )
+                dp[i][1] = min(
+                    dp[i-1][0] * N[i],
+                    dp[i-1][1] * N[i],
+                    N[i]
+                )
+                m = max(m, dp[i][0])
+            return m
+
+        # return dynamicProgramming(nums)
+
+        def dynamicProgramming_spaceOptmized(N):
+            dp_max, dp_min = 1, 1
+            m = N[0]
+            for i in range(len(N)):
+                dp_max, dp_min = (
+                    max(
+                        dp_max * N[i],
+                        dp_min * N[i],
+                        N[i]
+                    ), min(
+                        dp_max * N[i],
+                        dp_min * N[i],
+                        N[i]
+                    )
+                )
+                m = max(m, dp_max)
+            return m
+
+        return dynamicProgramming_spaceOptmized(nums)
