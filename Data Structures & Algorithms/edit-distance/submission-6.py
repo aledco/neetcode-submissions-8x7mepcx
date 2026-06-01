87d5f230542bbdd3987514c126e2563dc17cf378
Alexander Cooper <103967988+aledco@users.noreply.github.com>
2026-06-01 13:32:34 -0700

    Add: edit-distance - submission-6

---
+++ Data Structures & Algorithms/edit-distance/submission-6.py
@@ -0,0 +1,63 @@
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        
+        def dfs(w1, w2, i=0, j=0):
+            if i >= len(w1):
+                return len(w2)-j
+            elif j >= len(w2):
+                return len(w1)-i
+            
+            if w1[i] == w2[j]:
+                return dfs(w1, w2, i+1, j+1)
+            else: 
+                return 1 + min(
+                    dfs(w1, w2, i+1, j),
+                    dfs(w1, w2, i, j+1),
+                    dfs(w1, w2, i+1, j+1)
+                )
+
+        # return dfs(word1, word2)
+
+        def dynamicProgramming(w1, w2):
+            # dp[i][j] = minmum edit distance to convert w1[:i] into w2[:j]
+            m, n = len(w1), len(w2)
+            dp = [[0] * (n+1) for _ in range(m+1)]
+            for i in range(m+1):
+                dp[i][0] = i
+            for j in range(n+1):
+                dp[0][j] = j
+            for i in range(m):
+                for j in range(n):
+                    if w1[i] == w2[j]:
+                        dp[i+1][j+1] = dp[i][j]
+                    else:
+                        dp[i+1][j+1] = 1 + min(
+                            dp[i][j+1],
+                            dp[i+1][j],
+                            dp[i][j]
+                        )
+            return dp[m][n]
+        
+        # return dynamicProgramming(word1, word2)
+
+        def dynamicProgramming_spaceOptimized(w1, w2):
+            # dp[i][j] = minmum edit distance to convert w1[:i] into w2[:j]
+            m, n = len(w1), len(w2)
+            dp = [j for j in range(n+1)]
+            dp_next = [0] * (n+1)
+            for i in range(m):
+                dp_next[0] = i
+                for j in range(n):
+                    if w1[i] == w2[j]:
+                        dp_next[j+1] = dp[j]
+                    else:
+                        dp_next[j+1] = 1 + min(
+                            dp[j+1],
+                            dp_next[j],
+                            dp[j]
+                        )
+                dp = dp_next.copy()
+                    
+            return dp[n]
+    
+        return dynamicProgramming_spaceOptimized(word1, word2)
