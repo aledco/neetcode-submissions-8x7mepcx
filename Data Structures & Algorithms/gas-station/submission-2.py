b3a244ee5eeb61b17171cf3b0178505384b27a14
Alexander Cooper <103967988+aledco@users.noreply.github.com>
2026-06-06 09:26:04 -0700

    Add: gas-station - submission-2

---
+++ Data Structures & Algorithms/gas-station/submission-2.py
@@ -0,0 +1,46 @@
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        def bruteForce(gas, cost):
+            
+            def canCircuit(gas, cost, s):
+                
+                if gas[s] < cost[s]:
+                    return False
+                
+                n = len(gas)
+                g = gas[s] - cost[s]
+                i = (s + 1) % n
+                while i != s:
+                    g += gas[i] - cost[i]
+                    if g < 0:
+                        return False
+                    i = (i + 1) % n
+                return True
+            
+            for i in range(len(gas)):
+                if canCircuit(gas, cost, i):
+                    return i
+            return -1
+        
+        # return bruteForce(gas, cost)
+
+        def bfs(gas, cost):
+            from collections import deque
+
+            n = len(gas)
+            Q = deque()
+            for i in range(n):
+                if gas[i] >= cost[i]:
+                    Q.append((i, (i+1) % n, gas[i] - cost[i]))
+            
+            while len(Q) > 0:
+                s, i, g = Q.popleft()
+                if s == i:
+                    return s
+
+                if g + gas[i] >= cost[i]:
+                    Q.append((s, (i+1) % n, g + gas[i] - cost[i]))
+            
+            return -1
+        
+        return bfs(gas, cost)
