4e63ed1ca7f0a655402517ed1038f2d11382a2ca
Alexander Cooper <103967988+aledco@users.noreply.github.com>
2026-06-13 07:28:16 -0700

    Add: count-squares - submission-0

---
+++ Data Structures & Algorithms/count-squares/submission-0.py
@@ -0,0 +1,54 @@
+from collections import defaultdict
+
+class CountSquares:
+
+    def __init__(self):
+        self.xaxis = defaultdict(set) # (x, y): self.xaxis[x] = {y}
+        self.yaxis = defaultdict(set) # (x, y): self.yaxis[y] = {x}
+        self.duplicates = defaultdict(int) # counts how many of each point have been added
+
+    def add(self, point: List[int]) -> None:
+        x, y = point
+        self.xaxis[x].add(y)
+        self.yaxis[y].add(x)
+        self.duplicates[(x, y)] += 1
+
+    def count(self, point: List[int]) -> int:
+        x, y = point
+
+        c = 0
+        for y2 in self.xaxis[x]:
+            if y == y2:
+                continue
+            
+            # (x, y) -> (x, y2) form a vertical line
+            for x2 in self.yaxis[y]:
+                if x == x2:
+                    continue
+                
+                if abs(x2 - x) != abs(y2 - y):
+                    continue
+                
+                # (x, y) -> (x2, y) form a horizonal line of same length as (x, y) -> (x, y2)
+                if y2 in self.xaxis[x2]:
+                    # print(
+                    #     (x, y),
+                    #     (x, y2),
+                    #     (x2, y),
+                    #     (x2, y2)
+                    # )
+                    d = (
+                        max(
+                            1,
+                            self.duplicates[(x, y)]
+                        ) 
+                        * self.duplicates[(x, y2)]
+                        * self.duplicates[(x2, y)]
+                        * self.duplicates[(x2, y2)]
+                    )
+                    c += d
+
+        # print()
+        return c
+
+
