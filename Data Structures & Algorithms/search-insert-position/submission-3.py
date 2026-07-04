ba9f324afdf4febc054d6dd612b5ed2899bd0a64
Alexander Cooper <103967988+aledco@users.noreply.github.com>
2026-07-04 08:30:43 -0700

    Add: search-insert-position - submission-3

---
+++ Data Structures & Algorithms/search-insert-position/submission-3.py
@@ -0,0 +1,22 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        
+        def binSearch(N, t, i, j):
+            if i >= j:
+                return i
+            
+            m = (i + j) // 2
+            if N[m] == t:
+                return m
+            elif N[m] < t:
+                # if m == len(N)-1 or N[m+1] > t:
+                #     return m+1
+                return binSearch(N, t, m+1, j)
+            else:
+                # if m == 0 or N[m-1] < t:
+                #     return m
+                return binSearch(N, t, i, m)
+
+        return binSearch(
+            nums, target, 0, len(nums)
+        )
