3326d4ec9909c7a98ceb687f75b880d8bf1877ae
Alexander Cooper <103967988+aledco@users.noreply.github.com>
2026-07-05 21:16:57 -0700

    Add: search-in-rotated-sorted-array-ii - submission-4

---
+++ Data Structures & Algorithms/search-in-rotated-sorted-array-ii/submission-4.py
@@ -0,0 +1,26 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+        
+        def binSearch(N, t, i, j):
+            if i >= j:
+                return False
+            
+            m = (i + j) // 2
+            if N[m] == t:
+                return True
+            elif N[i] < N[m]:
+                if N[i] <= t <= N[m]:
+                    return binSearch(N, t, i, m)
+                else:
+                    return binSearch(N, t, m+1, j)
+            elif N[i] > N[m]:
+                if N[m] <= t <= N[j-1]:
+                    return binSearch(N, t, m+1, j)
+                else:
+                    return binSearch(N, t, i, m)
+            else:
+                while i < j and N[i] == N[m]:
+                    i += 1
+                return binSearch(N, t, i, j)
+            
+        return binSearch(nums, target, 0, len(nums))
