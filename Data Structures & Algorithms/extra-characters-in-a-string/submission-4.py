9954ed59c3b2e39b7a7e77a6603149ef55e90f88
Alexander Cooper <103967988+aledco@users.noreply.github.com>
2026-07-18 08:04:45 -0700

    Add: extra-characters-in-a-string - submission-4

---
+++ Data Structures & Algorithms/extra-characters-in-a-string/submission-4.py
@@ -0,0 +1,71 @@
+class Solution:
+    def minExtraChar(self, s: str, dictionary: List[str]) -> int:
+        
+        def dfs(s, T, i=0):
+            if i >= len(s):
+                return 0
+            
+            res = 1 + dfs(s, T, i+1) # do not use character i
+            for w in T.find_all(s, i): # try each macthing word
+                res = min(
+                    res,
+                    dfs(s, T, i+len(w))
+                )
+            return res
+
+        # return dfs(s, Trie(dictionary))
+
+        def dynamicProgramming_topDown(s, T):
+            cache = {}
+
+            def dfs(s, T, i=0):
+                nonlocal cache
+
+                if i in cache:
+                    return cache[i]
+                
+                if i >= len(s):
+                    return 0
+                
+                cache[i] = 1 + dfs(s, T, i+1) # do not use character i
+                for w in T.find_all(s, i): # try each macthing word
+                     cache[i] = min(
+                         cache[i],
+                        dfs(s, T, i+len(w))
+                    )
+                return cache[i]
+            
+            return dfs(s, T)
+            
+        return dynamicProgramming_topDown(s, Trie(dictionary))
+
+
+class Trie:
+    def __init__(self, words):
+        self.root = TrieNode("")
+        self._build(words)
+    
+    def find_all(self, s, i=0):
+        curr = self.root
+        while i < len(s) and s[i] in curr.children:
+            curr = curr.children[s[i]]
+            if curr.is_word:
+                yield curr.word
+            i += 1
+
+    def _build(self, words):
+        for w in words:
+            curr_node = self.root
+            curr_word = ""
+            for c in w:
+                curr_word += c
+                if c not in curr_node.children:
+                    curr_node.children[c] = TrieNode(curr_word)
+                curr_node = curr_node.children[c]
+            curr_node.is_word = True
+
+class TrieNode:
+    def __init__(self, word, is_word=False):
+        self.word = word
+        self.is_word = is_word
+        self.children = {}
