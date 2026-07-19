ac8c82e2dcbab0c8d8a2b3845f3281bd921c7715
Alexander Cooper <103967988+aledco@users.noreply.github.com>
2026-07-18 18:03:57 -0700

    Add: word-break-ii - submission-0

---
+++ Data Structures & Algorithms/word-break-ii/submission-0.py
@@ -0,0 +1,46 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        trie = Trie(wordDict)
+        
+        def dfs(s, T, i=0):
+            if i >= len(s):
+                yield []
+                return
+            
+            for w in T.find_all(s, i):
+                for res in dfs(s, T, i+len(w)):
+                    yield [w] + res
+            
+        return [" ".join(x) for x in dfs(s, trie)]
+
+
+
+class Trie:
+    def __init__(self, words):
+        self.root = TrieNode()
+        self._build(words)
+
+    def find_all(self, s, i=0):
+        curr = self.root
+        while i < len(s) and s[i] in curr.children:
+            curr = curr.children[s[i]]
+            if curr.is_word:
+                yield curr.word
+            i += 1
+        
+    def _build(self, words):
+        for w in words:
+            curr_node = self.root
+            curr_word = ""
+            for c in w:
+                curr_word += c
+                if c not in curr_node.children:
+                    curr_node.children[c] = TrieNode(curr_word)
+                curr_node = curr_node.children[c]
+            curr_node.is_word = True
+
+class TrieNode:
+    def __init__(self, word="", is_word=False):
+        self.word = word
+        self.is_word = is_word
+        self.children = {}
