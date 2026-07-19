61b8074140796871c595d3c71f29fddf8dab0f1a
Alexander Cooper <103967988+aledco@users.noreply.github.com>
2026-07-18 18:23:30 -0700

    Add: island-perimeter - submission-4

---
+++ Data Structures & Algorithms/island-perimeter/submission-4.py
@@ -0,0 +1,22 @@
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        def water(G, i, j):
+            if i < 0 or i >= len(G) or j < 0 or j >= len(G[i]):
+                return 1
+            return 1 - G[i][j]
+
+        def shores(G, i, j):
+            if water(G, i, j) == 1:
+                return 0
+            return (
+                water(G, i-1, j) +
+                water(G, i+1, j) +
+                water(G, i, j-1) +
+                water(G, i, j+1)
+            )
+
+        res = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[i])):
+                res += shores(grid, i, j)
+        return res
