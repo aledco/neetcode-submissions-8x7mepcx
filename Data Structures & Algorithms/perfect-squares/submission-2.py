4a61ec68465d728240e0c3d536c3a1566b9bd8b9
Alexander Cooper <103967988+aledco@users.noreply.github.com>
2026-07-21 20:41:47 -0700

    Add: perfect-squares - submission-2

---
+++ Data Structures & Algorithms/perfect-squares/submission-2.py
@@ -0,0 +1,22 @@
+class Solution:
+    def numSquares(self, n: int) -> int:
+        import math
+        from functools import cache
+
+        @cache
+        def dfs(n):
+            if n == 0:
+                return 0
+            
+            r = int(math.sqrt(n))
+            q = int(math.sqrt(r))
+            res = sys.maxsize
+            for i in range(r, q-1, -1):
+                res = min(
+                    res,
+                    1 + dfs(n-i*i)
+                )
+            return res
+        
+        return dfs(n)
+        
