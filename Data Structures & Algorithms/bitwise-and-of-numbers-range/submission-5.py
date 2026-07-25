244e477e83d3a7a7db691c49a05637c58ecc650c
Alexander Cooper <103967988+aledco@users.noreply.github.com>
2026-07-25 15:35:03 -0700

    Add: bitwise-and-of-numbers-range - submission-5

---
+++ Data Structures & Algorithms/bitwise-and-of-numbers-range/submission-5.py
@@ -0,0 +1,20 @@
+class Solution:
+    def rangeBitwiseAnd(self, left: int, right: int) -> int:
+        def stripLeadingZeroes(b):
+            i = 0
+            while i < len(b)-1 and b[i] == '0':
+                i += 1
+            return b[i:]
+
+        def helper(a, b):
+            if len(a) == 0 or len(b) == 0:
+                return '0' * max(len(a), len(b))
+
+            if len(a) != len(b) or a[0] != b[0] or a == '0' or b == '0':
+                return '0' * max(len(a), len(b))
+
+            return a[0] + helper(a[1:], b[1:])
+
+        a, b = bin(left).replace("0b", ""), bin(right).replace("0b", "")
+        c = helper(a, b)
+        return int(c, 2)
