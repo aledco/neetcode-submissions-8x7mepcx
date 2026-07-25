2edaf937056c33b71ea820d29424151adb527c44
Alexander Cooper <103967988+aledco@users.noreply.github.com>
2026-07-25 15:41:38 -0700

    Add: bitwise-and-of-numbers-range - submission-10

---
+++ Data Structures & Algorithms/bitwise-and-of-numbers-range/submission-10.py
@@ -0,0 +1,21 @@
+class Solution:
+    def rangeBitwiseAnd(self, left: int, right: int) -> int:
+        i = 0
+        while left != right:
+            left >>= 1
+            right >>= 1
+            i += 1
+        return left << i
+
+        # def helper(a, b):
+        #     if len(a) == 0 or len(b) == 0:
+        #         return '0' * max(len(a), len(b))
+
+        #     if len(a) != len(b) or a[0] != b[0] or a == '0' or b == '0':
+        #         return '0' * max(len(a), len(b))
+
+        #     return a[0] + helper(a[1:], b[1:])
+
+        # a, b = bin(left).replace("0b", ""), bin(right).replace("0b", "")
+        # c = helper(a, b)
+        # return int(c, 2)
