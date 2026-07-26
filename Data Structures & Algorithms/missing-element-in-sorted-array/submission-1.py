958a31c56eadf8ff8509abccd1f5ad902c684a17
Alexander Cooper <103967988+aledco@users.noreply.github.com>
2026-07-26 07:03:00 -0700

    Add: missing-element-in-sorted-array - submission-1

---
+++ Data Structures & Algorithms/missing-element-in-sorted-array/submission-1.py
@@ -0,0 +1,23 @@
+class Solution:
+    def missingElement(self, nums: List[int], k: int) -> int:
+        
+        def divide(nums, i, j, k):
+            print("i, j =", i, j)
+
+            if j - i + 1 == 2:
+                # kth must be in between nums[i] and nums[j]
+                res = nums[i] + k
+                if res >= nums[j]: # if kth is after the end of nums, need to account for nums[j] existing in the range of nums[i] to kth
+                    res += 1
+                return res
+
+            m = (i + j) // 2
+            missing_left = (nums[m] - nums[i] + 1) - (m - i + 1)
+            print("m, k, missing_left =", m, k, missing_left)
+            if missing_left >= k:
+                return divide(nums, i, m, k)
+            else:
+                return divide(nums, m, j, k - missing_left)
+
+
+        return divide(nums, 0, len(nums)-1, k)
